232ac02dcf22936fde3959eb6ea32279ab375f5a
FazlOmar9 <bt22csa026@iiitn.ac.in>
2023-07-17 15:38:11 +0530

    Removed bug
    Removed bug that stopped user from getting redirected to the login page upon a failed login attempt, after displaying wrong id / pass.

--- main_proj/login.php
+++ main_proj/login.php
@@ -16,6 +16,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     } else {
     // Login failed
     echo "<script>window.alert('Incorrect email or password')</script>";
+    echo "<script>window.location.href='index.php'</script>";
     }
     // Close the database connection
     $conn->close();
